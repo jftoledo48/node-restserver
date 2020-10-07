f50e9b3f839a11be46ac417665befb26acd316c6
Juan Francisco Toledo <juan.fco.toledo@gmail.com>
2020-10-07 12:45:52 -0300

    Primer commit

---
+++ server/config/config.js
@@ -0,0 +1,5 @@
+// =======================================================
+// Puerto: Config of port Developer and Production Ambient
+// =======================================================
+
+process.env.PORT = process.env.PORT || 3000
